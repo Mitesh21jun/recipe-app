211b7af0d3b0031b10f650a41a211756fa97f696
Mitesh21jun <msahu21jun@gmail.com>
2021-04-18 13:23:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Recipe from "./Recipe";
+const App = () => {
+  //variables
+  const APP_ID = "6db2d6c2";
+  const APP_KEY = "5fa585ec7f0225d8f1a5d8097404307f";
+  //useStates
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState(Math.floor(Math.random()*100));
+
+  const URL = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  //useEffects
+  useEffect(() => {
+    getRecipes();
+  }, [query]);
+
+  //methods
+  const getRecipes = async () => {
+    const response = await fetch(URL);
+    const data = await response.json();
+    setRecipes(data.hits);
+    console.log(data.hits);
+    setSearch("");
+  };
+
+  const updateSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getSearch = (e) => {
+    e.preventDefault();
+    setQuery(search);
+  };
+
+  return (
+    <div className="App">
+      <div className="container">
+      <form onSubmit={getSearch} className="search-form">
+        <input
+          type="text"
+          className="search-bar"
+          value={search}
+          onChange={updateSearch}
+        />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
+      <div className="container-fluid">
+        {recipes.map((recipe) => (
+          <Recipe
+            key={recipe.recipe.label}
+            title={recipe.recipe.label}
+            calories={recipe.recipe.calories}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+          />
+        ))}
+      </div></div>
+    </div>
+  );
+};
+export default App;
---
+++ src/Recipe.js
@@ -0,0 +1,30 @@
+import React from "react";
+import style from "./recipe.module.css";
+// import { Card } from "react-bootstrap";
+// import { Button } from "react-bootstrap";
+
+const Recipe = ({ title, calories, image, ingredients }) => {
+  return (
+    <div className="col-lg-4 col-md-4 col-sm-6 pb-2 d-inline-flex">
+      <div className="row">
+        <div className="">
+          <div className="card pr-2" style={{width : "20rem", boxShadow: '0px 3px 10px rgb(71, 71, 71)'}}>
+          <img src={image} className="card-img-top m-1" alt="..."/>
+
+            <div className="card-body">
+              <h5 className="card-title">{title}</h5>
+              <p className="lead">Calories :{calories}</p>
+
+              <ul className="list-styled">
+                {ingredients.map((ingredient) => (
+                  <li className="text-wrap">{ingredient.text}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Recipe;
