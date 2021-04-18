c4491e51e92b9bea9bfd912ad050211c9f7a82ba
Mitesh21jun <msahu21jun@gmail.com>
2021-04-18 14:11:40 +0530

    alignment

--- src/Recipe.js
+++ src/Recipe.js
@@ -5,10 +5,10 @@ import React from "react";
 
 const Recipe = ({ title, calories, image, ingredients }) => {
   return (
-    <div className="col-lg-4 col-md-4 col-sm-6 pb-2 d-inline-flex">
+    <div className="col-lg-4 col-md-6 col-sm-12 pb-2 d-inline-flex">
       <div className="row">
         <div className="">
-          <div className="card pr-2" style={{width : "20rem", boxShadow: '0px 3px 10px rgb(71, 71, 71)'}}>
+          <div className="card pr-3 p-2 ml-3" style={{width : "20rem", boxShadow: '0px 3px 10px rgb(71, 71, 71)'}}>
           <img src={image} className="card-img-top m-1" alt="..."/>
 
             <div className="card-body">
